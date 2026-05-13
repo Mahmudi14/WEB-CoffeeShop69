bcc8108546cf9a4308334b24f339a28ffb4e0126
Mahmuddin <genzxx14@gmail.com>
2026-05-14 01:35:59 +0800

    Update modul POS

--- resources/views/cashier/pos/index.blade.php
+++ resources/views/cashier/pos/index.blade.php
@@ -190,7 +190,7 @@
             <aside
                 class="min-w-0 self-start min-[768px]:sticky min-[768px]:top-24 min-[768px]:z-20 min-[768px]:max-h-[calc(100dvh-10rem)]">
                 <div
-                    class="flex w-full flex-col overflow-hidden rounded-[2rem] border border-stone-100 bg-white shadow-sm min-[768px]:max-h-[calc(100dvh-7rem)]">
+                    class="flex w-full flex-col overflow-hidden rounded-[2rem] border border-stone-100 bg-white shadow-sm min-[768px]:max-h-[calc(100dvh-10rem)]">
                     {{-- Cart Header --}}
                     <div class="shrink-0 border-b border-stone-100 p-4 xl:p-5">
                         <div class="flex items-center justify-between gap-4">
