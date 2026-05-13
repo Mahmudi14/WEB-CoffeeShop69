7c2dc22f65935328eb4472e79c5a41acda34c0ec
Mahmuddin <genzxx14@gmail.com>
2026-05-14 04:14:31 +0800

    Update modul POS

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/', function () {
     return redirect()
         ->route('login')
         ->with('error', 'Akun belum memiliki role yang valid.');
-});
+})->middleware('no-cache');
 
 
 /*
